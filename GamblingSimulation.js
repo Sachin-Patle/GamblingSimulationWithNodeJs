bf2c063206b1a908d2ba2d9f3b8554ab2bffdcb7
Sachin-Patle <patlesachin1@gmail.com>
2020-12-03 13:14:44 +0530

    Added welcome message to start the program

---
+++ GamblingSimulation.js
@@ -0,0 +1,10 @@
+class GamblingSimulation
+{
+    constructor()
+    {
+        console.log("=======================================");
+        console.log("Welcome to Gambling Simulation Problem")
+        console.log("=======================================");
+    }
+}
+let gambler=new GamblingSimulation();
