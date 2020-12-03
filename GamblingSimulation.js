ef30084b5568ad37675de469069bb72dd968ca97
Sachin-Patle <patlesachin1@gmail.com>
2020-12-03 14:02:53 +0530

    Started Stake and Bet and checking win or loss by random number

--- GamblingSimulation.js
+++ GamblingSimulation.js
@@ -7,6 +7,12 @@
 class GamblingSimulation
 {
     /**
+     * Variable Declarations
+     */
+    static stake=100;
+    static bet=1;
+
+    /**
      * @constructor
      * @description Displays welcome message
      */
@@ -16,5 +22,26 @@ class GamblingSimulation
         console.log("Welcome to Gambling Simulation Problem")
         console.log("=======================================");
     }
+    
+    /**
+     * Method to check either win or loss
+     * 
+     * @method win_or_loss
+     * @description Check win or loss
+     */
+    win_or_loss()
+    {
+        var random = Math.floor(Math.random() * 2);
+        
+        if(random==1)
+        {
+            console.log("Win !");
+        }
+        else
+        {
+            console.log("Loss !");
+        }
+    }
 }
-let gambler=new GamblingSimulation();
+let gambler_object=new GamblingSimulation();
+gambler_object.win_or_loss();
